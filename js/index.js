44f67601465bbbbb5850038506888a94e59f402a
Marcus Jones <indelibleink84@gmail.com>
2019-06-18 09:08:35 -0500

    WIP: Trying to figure out the correct syntax and how to properly formulate code

--- js/index.js
+++ js/index.js
@@ -39,4 +39,63 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+//Nav Section
+let navLinks = document.querySelectorAll('nav a');
+navLinks[1].innerHTML = siteContent['nav']['nav-item-1'];
+navLinks[2].innerHTML = siteContent['nav']['nav-item-2'];
+navLinks[3].innerHTML = siteContent['nav']['nav-item-3'];
+navLinks[4].innerHTML = siteContent['nav']['nav-item-4'];
+navLinks[5].innerHTML = siteContent['nav']['nav-item-5'];
+
+let circleLogo = document.getElementById('cta-img');
+circleLogo.setAttribute('src', siteContent['cta']['img-src']); 
+
+
+let button = document.querySelector('button');
+button.innerHTML = 'Get Started';
+
+//updated main contect section
+let paragraph = document.querySelectorAll('p');
+paragraph[1].innerHTML = siteContent ['main-content'] ['features-content'];
+paragraph[2].innerHTML = siteContent ['main-content'] ['about-content'];
+paragraph[3].innerHTML = siteContent ['main-content'] ['services-content'];
+paragraph[4].innerHTML = siteContent ['main-content'] ['product-content'];
+paragraph[5].innerHTML = siteContent ['main-content'] ['middle-img-src'];
+paragraph[6].innerHTML = siteContent ['contact'] ['address'];
+paragraph[7].innerHTML = siteContent ['contact'] ['phone'];
+paragraph[8].innerHTML = siteContent ['footer'] ['copyright'];
+
+let midImage= document.getElementById('img');
+midImage.setAttribute('src', siteContent['main-content']['img/mid-page-accent.jpg']); 
+
+
+//updated h4 section
+let h4Titles = document.querySelectorAll('h4');
+h4Titles[1].innerHTML = siteContent ['main-content'] ['features-h4'];
+h4Titles[2].innerHTML = siteContent ['main-content'] ['about-h4'];
+h4Titles[3].innerHTML = siteContent ['main-content'] ['services-h4'];
+h4Titles[4].innerHTML = siteContent ['main-content'] ['product-h4'];
+h4Titles[5].innerHTML = siteContent ['main-content'] ['vision-h4'];
+h4Titles[6].innerHTML = siteContent ['contact'] ['contact-h4'];
+h4Titles[7].innerHTML = siteContent ['contact'] ['email'];
+
+
+let title = document.querySelectorAll('h1');
+title[1].innerHTML = siteContent['h1'];
+
+
+
+
+
+
+let contact = document.querySelectorAll('contact');
+
+
+
+
+
+
+
+
